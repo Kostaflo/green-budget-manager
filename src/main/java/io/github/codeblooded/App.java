2e94b74783aeaf7eb8f05acb82e38b5fe76401bb
Agapis Giannis <t8240001@aueb.gr>
2025-11-12 00:15:11 +0200

    updated pom.xml for junit and test changes in both App.java and Apptest.java

--- src/main/java/io/github/codeblooded/App.java
+++ src/main/java/io/github/codeblooded/App.java
@@ -1,13 +1,13 @@
 package io.github.codeblooded;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Green Budget Manager!");
+        System.out.println("2 + 3 = " + addNumbers(2, 3));
     }
 }
+
