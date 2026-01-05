cc1b512bc3901217cf2362fcee848ed4e951ef31
AfroditiKotsialou <t8220067@aueb.gr>
2026-01-05 19:47:38 +0200

    Finalize test package structure

--- src/test/java/io/github/codeblooded/model/GreenBudgetTagTest.java
+++ src/test/java/io/github/codeblooded/model/GreenBudgetTagTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import io.github.codeblooded.model.GreenBudgetTag;
 
 public class GreenBudgetTagTest {
 
